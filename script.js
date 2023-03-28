d03ca99f9da46fcc801a6c99afb5842235f78631
Auto Logon <autologon@CSED.LAB>
2023-03-28 17:55:10 -0300

    requicao para api

---
+++ script.js
@@ -0,0 +1,24 @@
+function inserirTituloNaDiv() {
+    let meuArray = [1,2,4,5]
+        {
+            nome: 'julio'
+            idade: '18'
+            email:'mgd@gmail'
+        
+                console.log('Meu Nome é Júlio')
+
+        }
+}
+
+
+
+
+
+ 
+   
+
+
+
+
+   
+
